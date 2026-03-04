4359dbd05baf348451bbc9d5225fd68472a7926d
Yedidya Hess-Green <yedidya.magshimim@gmail.com>
2026-03-04 22:26:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ with tab1:
         # הגדרת הצבעים לפי הסדר שביקשת (מהנמוך לגבוה)
         # ירוק כהה (מעט אזעקות) -> ירוק בהיר -> צהוב -> כתום -> אדום (הרבה אזעקות)
         colormap = cm.LinearColormap(
-            colors=['#006400', '#90EE90', '#FFFF00', '#FFA500', '#FF0000'],
+            colors=['#006400', '#90EE90', '#FFFF00', '#FF8C00', '#800026'],
             vmin=float(final_df["alarm_count"].min()), 
             vmax=float(final_df["alarm_count"].max()),
             caption='כמות אזעקות'
@@ -129,6 +129,7 @@ with tab3:
         st.write(f"- {city2}: {len(comp_df[comp_df['cities'] == city2])} אזעקות")
     else:
         st.info("אין מספיק נתונים להשוואה בטווח ובסינון שנבחר.")
+
 
 
 
